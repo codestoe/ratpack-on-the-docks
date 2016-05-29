3f95739136e1c94c821af3afa3f81861d2db41c4
noamt <noam@10ne.org>
2016-05-29 16:50:50 +0300

    Adding env scripts to every module

---
+++ abuse/setenv.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export ARGUMENT_NODES="http://localhost:5052"
+export PORT=5051
---
+++ secretary/setenv.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export ABUSE_NODES="http://localhost:5051"
+export ARGUMENT_NODES="http://localhost:5052"
